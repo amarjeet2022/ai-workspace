b50c4d20ea6337156013df8c1cd3841ea163b0e6
Amarjeet <javaqsu177777@gmail.com>
2026-07-03 12:11:15 +0530

    Api phase-1

--- BackendAI/main.py
+++ BackendAI/main.py
@@ -4,3 +4,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hellow world"}
+
+@app.get("/user")
+def user():
+    return [
+        {"id": 1, "name": "Amarjeet"},
+        {"id": 2, "name": "Aman"},
+
+    ]
